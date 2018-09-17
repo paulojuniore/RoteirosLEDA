bd676495bcc110214bd23a944dc7fde0ff937099
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-09-17 13:50:15 -0300

    Modificações no CountingSort e ExtendedCountingSort

--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -14,34 +14,33 @@ public class CountingSort extends AbstractSorting<Integer> {
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
 
-		if (array.length == 0)
-			return;
+		if (array.length == 0) {
+			/** Encontra o maior elemento do array e aloca um array com este valor + 1. **/
+			int max = Util.getMaximum(array, leftIndex, rightIndex);
+			Integer[] b = new Integer[max + 1];
+			Util.initializeVectorWithZero(b);
+			
+			/** Conta a ocorrência para cada elemento no array. **/
+			for (int j = leftIndex; j <= rightIndex; j++) {
+				b[array[j]]++;
+			}
 
-		/** Encontra o maior elemento do array e aloca um array com este valor + 1. **/
-		int max = Util.getMaximum(array, leftIndex, rightIndex);
-		Integer[] b = new Integer[max + 1];
-		Util.initializeVectorWithZero(b);
-		
-		/** Conta a ocorrência para cada elemento no array. **/
-		for (int j = leftIndex; j <= rightIndex; j++) {
-			b[array[j]]++;
-		}
+			/** Modifica a contagem de modo que as posições no final do array sejam obtidas.  **/
+			for (int j = leftIndex + 1; j < max + 1; j++) {
+				b[j] += b[j - 1];
+			}
+			
+			/** Aloca um vetor temporário para guardar o array original **/
+			Integer[] temp = new Integer[array.length];
+			for (int i = array.length - 1; i >= 0; i--) {
+				temp[b[array[i]] - 1] = array[i];
+				b[array[i]]--;
+			}
 
-		/** Modifica a contagem de modo que as posições no final do array sejam obtidas.  **/
-		for (int j = leftIndex + 1; j < max + 1; j++) {
-			b[j] += b[j - 1];
-		}
-		
-		/** Aloca um vetor temporário para guardar o array original **/
-		Integer[] temp = new Integer[array.length];
-		for (int i = array.length - 1; i >= 0; i--) {
-			temp[b[array[i]] - 1] = array[i];
-			b[array[i]]--;
-		}
-
-		/** Transfere o vetor temporário para o array original **/
-		for (int i = leftIndex; i <= rightIndex; i++) {
-			array[i] = temp[i];
+			/** Transfere o vetor temporário para o array original **/
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				array[i] = temp[i];
+			}
 		}
 
 	}
--- R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -14,35 +14,32 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
 
-		if (array.length == 0)
-			return;
+		if (array.length == 0) {
+			/** Encontra o elemento máximo e mínimo no array **/
+			int min = Util.getMinimum(array, leftIndex, rightIndex);
+			int max = Util.getMaximum(array, leftIndex, rightIndex);
+			int range = max - min + 1;
+			Integer[] b = new Integer[range];
+			Util.initializeVectorWithZero(b);
 
-		/** Encontra o elemento máximo e mínimo no array **/
-		int min = Util.getMinimum(array, leftIndex, rightIndex);
-		int max = Util.getMaximum(array, leftIndex, rightIndex);
-		int range = max - min + 1;
-		Integer[] b = new Integer[range];
-		Util.initializeVectorWithZero(b);
+			/** Faz a contagem da frequência para cada elemento do array **/
+			for (int i = leftIndex; i <= rightIndex; i++) {
+				b[array[i] - min]++;
+			}
 
-		/** Faz a contagem da frequência para cada elemento do array **/
-		for (int i = leftIndex; i <= rightIndex; i++) {
-			b[array[i] - min]++;
+			/**
+			 * Modifica a contagem de modo que as posições no final do array sejam obtidas.
+			 **/
+			for (int i = leftIndex + 1; i < range; i++) {
+				b[i] += b[i - 1];
+			}
+
+			/** modifica o array original **/
+			int j = 0;
+			for (int i = 0; i < range; i++) {
+				while (j < b[i])
+					array[j++] = i + min;
+			}
 		}
-
-		/**
-		 * Modifica a contagem de modo que as posições no final do array sejam obtidas.
-		 **/
-		for (int i = leftIndex + 1; i < range; i++) {
-			b[i] += b[i - 1];
-		}
-
-		/** modifica o array original **/
-		int j = 0;
-		for (int i = 0; i < range; i++) {
-			while (j < b[i])
-				array[j++] = i + min;
-		}
-
 	}
-
 }
