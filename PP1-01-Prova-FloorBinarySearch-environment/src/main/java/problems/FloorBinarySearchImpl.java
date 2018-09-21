45059eefe0e19d496ff9d1dbc1546e3f1f0ddba4
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-09-21 09:30:01 -0300

    FloorBinarySearch implementado

--- PP1-01-Prova-FloorBinarySearch-environment/src/main/java/problems/FloorBinarySearchImpl.java
+++ PP1-01-Prova-FloorBinarySearch-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -10,18 +10,37 @@ public class FloorBinarySearchImpl implements Floor {
 	}
 	
 	private Integer floorBinarySearch(Integer[] array, int ini, int fim, int x) {
-		if(ini > fim)
+		Integer retorno = null;
+		if(ini <= fim) {
+			int meio = (ini+fim)/2;
+			if(x == array[meio]) {
+				retorno = array[meio];
+			} else if(x < array[meio]) {
+				if(array[meio-1] < x) {
+					retorno = array[meio-1];
+				} else {
+					retorno = floorBinarySearch(array, ini, meio-1, x);
+				}
+			} else {
+				retorno = floorBinarySearch(array, meio+1, fim, x);
+			}
+		}
+		return retorno;
+	}
+	
+	private Integer floorBinarySearch2(Integer[] array, int ini, int fim, int x) {
+		if (ini > fim)
 			return null;
-		if(x >= array[fim])
+		if (x >= array[fim])
 			return array[fim];
-		int meio = (ini+fim)/2;
-		if(array[meio] == x)
+		int meio = (ini + fim) / 2;
+		if (array[meio] == x)
 			return array[meio];
-		if(meio > 0 && array[meio-1] <= x && x < array[meio])
-			return array[meio-1];
-		if(x < array[meio])
-			return floorBinarySearch(array, ini, meio-1, x);
-		return floorBinarySearch(array, meio+1, fim, x);
+		if (meio > 0 && array[meio - 1] <= x && x < array[meio])
+			return array[meio - 1];
+		if (x < array[meio])
+			return floorBinarySearch(array, ini, meio - 1, x);
+		return floorBinarySearch(array, meio + 1, fim, x);
 	}
 	
 	private void insertionSort(Integer[] array, int leftIndex, int rightIndex) {
--- PP1-01-Prova-FloorBinarySearch-environment/src/main/java/problems/Main.java
+++ PP1-01-Prova-FloorBinarySearch-environment/src/main/java/problems/Main.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Integer[] array = {1, 2, 3, 4, 5, 6};
 		Integer[] array2 = {3, 1, 7, 0, 5, 8, 9};
 		System.out.println(Arrays.toString(array2));
 		FloorBinarySearchImpl f = new FloorBinarySearchImpl();
-		Integer index = f.floor(array2, 5);
+		Integer index = f.floor(array2, 6);
 		System.out.println(Arrays.toString(array2));
 		System.out.println(index);
 	}
