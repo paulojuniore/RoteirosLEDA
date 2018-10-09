1929054b169cf4dd4cb61d5997d82612516180e2
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-10-09 13:19:46 -0300

    LinkedList Recursiva

--- R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/stack/StackDoubleLinkedListImpl.java
+++ R06-01-Rot-Linked-list-abordagem-iterativa-environment/src/main/java/adt/stack/StackDoubleLinkedListImpl.java
@@ -2,54 +2,53 @@ package adt.stack;
 
 import adt.linkedList.DoubleLinkedList;
 import adt.linkedList.DoubleLinkedListImpl;
-import adt.linkedList.DoubleLinkedListNode;
 import adt.linkedList.SingleLinkedListImpl;
 
 public class StackDoubleLinkedListImpl<T> implements Stack<T> {
 
-	protected DoubleLinkedList<T> top;
-	protected int size;
+   protected DoubleLinkedList<T> top;
+   protected int size;
 
-	public StackDoubleLinkedListImpl(int size) {
-		this.size = size;
-		this.top = new DoubleLinkedListImpl<T>();
-	}
+   public StackDoubleLinkedListImpl(int size) {
+      this.size = size;
+      this.top = new DoubleLinkedListImpl<T>();
+   }
 
-	@Override
-	public void push(T element) throws StackOverflowException {
-		if(!isFull()) {
-			top.insertFirst(element);
-		} else {
-			throw new StackOverflowException();
-		}
-	}
+   @Override
+   public void push(T element) throws StackOverflowException {
+      if (!isFull()) {
+         top.insertFirst(element);
+      } else {
+         throw new StackOverflowException();
+      }
+   }
 
-	@Override
-	public T pop() throws StackUnderflowException {
-		if(!isEmpty()) {
-			T retorno = (T) ((SingleLinkedListImpl<T>) top).getHead();
-			top.removeFirst();
-			return retorno;
-		}
-		throw new StackUnderflowException();
-	}
+   @Override
+   public T pop() throws StackUnderflowException {
+      if (!isEmpty()) {
+         T retorno = (T) ((SingleLinkedListImpl<T>) top).getHead();
+         top.removeFirst();
+         return retorno;
+      }
+      throw new StackUnderflowException();
+   }
 
-	@Override
-	public T top() {
-		if(!isEmpty()) {
-			return (T) ((SingleLinkedListImpl<T>) top).getHead();
-		}
-		return null;
-	}
+   @Override
+   public T top() {
+      if (!isEmpty()) {
+         return (T) ((SingleLinkedListImpl<T>) top).getHead();
+      }
+      return null;
+   }
 
-	@Override
-	public boolean isEmpty() {
-		return ((SingleLinkedListImpl<T>) top).getHead().isNIL();
-	}
+   @Override
+   public boolean isEmpty() {
+      return ((SingleLinkedListImpl<T>) top).getHead().isNIL();
+   }
 
-	@Override
-	public boolean isFull() {
-		return size == top.size();
-	}
+   @Override
+   public boolean isFull() {
+      return size == top.size();
+   }
 
 }
