4585019e401842069d40cf4af41d45f13fa87d95
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-10-18 12:15:18 -0300

    HashTableOpenAdressLinearProbing implementada.

--- R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ R09-01-Rot-TabelaHash-EndAberto-environment/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,6 +2,7 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
 public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 		AbstractHashtableOpenAddress<T> {
@@ -15,26 +16,71 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int i = 0;
+		while(i < this.table.length) {
+			int j = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, i);
+			if(this.table[j] == null || this.table[j].equals(new DELETED())) {
+				this.table[j] = element;
+				this.elements++;
+				return;
+			} else {
+				i++;
+				this.COLLISIONS++;
+			}
+		}
+		throw new HashtableOverflowException();
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int i = 0;
+		while(i < this.table.length) {
+			int j = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, i);
+			if(this.table[j] == null) {
+				break;
+			} else if(this.table[j].equals(element)) {
+				this.table[j] = new DELETED();
+				this.elements--;
+			} else {
+				i++;
+			}
+		}
+		
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int i = 0;
+		T retorno = null;
+		int j;
+		while(i < this.table.length) {
+			j = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, i);
+			if(this.table[j] == null) {
+				break;
+			} else if(this.table[j].equals(element)) {
+				retorno = (T) this.table[j];
+				break;
+			}
+			i++;
+		}
+		return retorno;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int i = 0;
+		int saida = -1;
+		int j;
+		do {
+			j = ((HashFunctionOpenAddress<T>) this.hashFunction).hash(element, i);
+			if(this.table[j].equals(element)) {
+				saida = j;
+				break;
+			} else {
+				i++;
+			}
+		} while (this.table[j] == null || i < this.table.length);
+		return saida;	
 	}
 
 }
