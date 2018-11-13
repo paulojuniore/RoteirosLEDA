9d6cc4d79c9254113192b8bdf31364ef06b7369e
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-11-13 11:03:53 -0300

    Quase lá :'(

--- R14-01-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
+++ R14-01-Rot-ArvoreAVLCountFill-environment/src/main/java/adt/avltree/AVLCountAndFillImpl.java
@@ -1,7 +1,13 @@
 package adt.avltree;
 
-public class AVLCountAndFillImpl<T extends Comparable<T>> extends
-		AVLTreeImpl<T> implements AVLCountAndFill<T> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import adt.bst.BSTNode;
+import adt.bt.Util;
+
+public class AVLCountAndFillImpl<T extends Comparable<T>> extends AVLTreeImpl<T> implements AVLCountAndFill<T> {
 
 	private int LLcounter;
 	private int LRcounter;
@@ -9,7 +15,7 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 	private int RLcounter;
 
 	public AVLCountAndFillImpl() {
-		
+
 	}
 
 	@Override
@@ -33,9 +39,49 @@ public class AVLCountAndFillImpl<T extends Comparable<T>> extends
 	}
 
 	@Override
+	protected void rebalance(BSTNode<T> node) {
+		if (!node.isEmpty()) {
+			if (calculateBalance(node) > 1) {
+				if (calculateBalance((BSTNode<T>) node.getLeft()) >= 0) {
+					Util.rightRotation(node);
+					LLcounter++;
+				} else {
+					Util.leftRotation((BSTNode<T>) node.getLeft());
+					Util.rightRotation(node);
+					LRcounter++;
+				}
+			}
+			if (calculateBalance(node) < -1) {
+				if (calculateBalance((BSTNode<T>) node.getRight()) <= 0) {
+					Util.leftRotation(node);
+					RRcounter++;
+				} else {
+					Util.rightRotation((BSTNode<T>) node.getRight());
+					Util.leftRotation(node);
+					RLcounter++;
+				}
+			}
+		}
+	}
+
+	@Override
 	public void fillWithoutRebalance(T[] array) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		List<T[]> list = new ArrayList<T[]>();
+		Arrays.sort(array);
+		list.add(array);
+		int i = 0;
+		while (i < list.size()) {
+			T[] arrayAux = list.get(i);
+			int middle = arrayAux.length / 2;
+			T[] arrayAux1 = Arrays.copyOfRange(arrayAux, 0, middle);
+			T[] arrayAux2 = Arrays.copyOfRange(arrayAux, middle + 1, arrayAux.length);
+			if (arrayAux.length > 1) {
+				list.add(arrayAux1);
+				list.add(arrayAux2);
+			}
+			insert(arrayAux[middle]);
+			i++;
+		}
 	}
 
 }
