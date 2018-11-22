e3ee0bf9f04c60d03b8f1a901122f1f8de22e405
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-11-22 15:38:59 -0300

    roteiro 15

--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/bt/Util.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/bt/Util.java
@@ -4,25 +4,52 @@ import adt.bst.BSTNode;
 
 public class Util {
 
-
 	/**
 	 * A rotacao a esquerda em node deve subir e retornar seu filho a direita
+	 * 
 	 * @param node
 	 * @return
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> leftRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> filhoDireita = (BSTNode<T>) node.getRight();
+
+		filhoDireita.setParent(node.getParent());
+		if (filhoDireita.getParent() != null) {
+			if (node.getParent().getRight().equals(node)) {
+				node.getParent().setRight(filhoDireita);
+			} else {
+				node.getParent().setLeft(filhoDireita);
+			}
+		}
+		node.setRight(filhoDireita.getLeft());
+		filhoDireita.getLeft().setParent(node);
+		filhoDireita.setLeft(node);
+		node.setParent(filhoDireita);
+		return filhoDireita;
 	}
 
 	/**
 	 * A rotacao a direita em node deve subir e retornar seu filho a esquerda
+	 * 
 	 * @param node
 	 * @return
 	 */
 	public static <T extends Comparable<T>> BSTNode<T> rightRotation(BSTNode<T> node) {
-		//TODO Implemente sua rotacao a esquerda aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		BSTNode<T> filhoEsquerda = (BSTNode<T>) node.getLeft();
+
+		filhoEsquerda.setParent(node.getParent());
+		if (filhoEsquerda.getParent() != null) {
+			if (node.getParent().getLeft().equals(node)) {
+				node.getParent().setLeft(filhoEsquerda);
+			} else {
+				node.getParent().setRight(filhoEsquerda);
+			}
+		}
+		node.setLeft(filhoEsquerda.getRight());
+		filhoEsquerda.getRight().setParent(node);
+		filhoEsquerda.setRight(node);
+		node.setParent(filhoEsquerda);
+		return filhoEsquerda;
 	}
 
 	public static <T extends Comparable<T>> T[] makeArrayOfComparable(int size) {
--- R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
+++ R16-01-Rot-ArvorePV-environment/src/main/java/adt/rbtree/RBTreeImpl.java
@@ -1,10 +1,11 @@
 package adt.rbtree;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
+import adt.bt.Util;
 import adt.rbtree.RBNode.Colour;
 
-public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
-		implements RBTree<T> {
+public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T> implements RBTree<T> {
 
 	public RBTreeImpl() {
 		this.root = new RBNode<T>();
@@ -16,16 +17,15 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 	}
 
 	protected boolean verifyProperties() {
-		boolean resp = verifyNodesColour() && verifyNILNodeColour()
-				&& verifyRootColour() && verifyChildrenOfRedNodes()
+		boolean resp = verifyNodesColour() && verifyNILNodeColour() && verifyRootColour() && verifyChildrenOfRedNodes()
 				&& verifyBlackHeight();
 
 		return resp;
 	}
 
 	/**
-	 * The colour of each node of a RB tree is black or red. This is guaranteed
-	 * by the type Colour.
+	 * The colour of each node of a RB tree is black or red. This is guaranteed by
+	 * the type Colour.
 	 */
 	private boolean verifyNodesColour() {
 		return true; // already implemented
@@ -47,8 +47,8 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 	}
 
 	/**
-	 * Verifies the property for all RED nodes: the children of a red node must
-	 * be BLACK.
+	 * Verifies the property for all RED nodes: the children of a red node must be
+	 * BLACK.
 	 */
 	private boolean verifyChildrenOfRedNodes() {
 		// TODO Implement your code here
@@ -65,8 +65,7 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 
 	@Override
 	public void insert(T value) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		
 	}
 
 	@Override
@@ -77,27 +76,55 @@ public class RBTreeImpl<T extends Comparable<T>> extends BSTImpl<T>
 
 	// FIXUP methods
 	protected void fixUpCase1(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (node.equals(root)) {
+			node.setColour(Colour.BLACK);
+		} else {
+			fixUpCase2(node);
+		}
 	}
 
 	protected void fixUpCase2(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (((RBNode<T>) node.getParent()).getColour() == Colour.BLACK) {
+
+		} else {
+			fixUpCase3(node);
+		}
 	}
 
 	protected void fixUpCase3(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		RBNode<T> aux = (RBNode<T>) node.getParent().getParent();
+		if (((RBNode<T>) aux.getRight()).getColour() == Colour.RED) {
+			((RBNode<T>) aux.getLeft()).setColour(Colour.BLACK);
+			((RBNode<T>) aux.getRight()).setColour(Colour.BLACK);
+			aux.setColour(Colour.RED);
+			fixUpCase1(node);
+		} else {
+			fixUpCase4(node);
+		}
 	}
 
 	protected void fixUpCase4(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		RBNode<T> next = node;
+		RBNode<T> parent = (RBNode<T>) node.getParent();
+		RBNode<T> grandparent = (RBNode<T>) parent.getParent();
+		if(parent.getRight().equals(node) && grandparent.getLeft().equals(parent)) {
+			Util.leftRotation(parent);
+			next = (RBNode<T>) node.getLeft();
+		} else if(parent.getLeft().equals(node) && grandparent.getRight().equals(parent)) {
+			Util.rightRotation(parent);
+			next = (RBNode<T>) node.getRight();
+		}
+		fixUpCase5(next);
 	}
 
 	protected void fixUpCase5(RBNode<T> node) {
-		// TODO Implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		RBNode<T> parent = (RBNode<T>) node.getParent();
+		parent.setColour(Colour.BLACK);
+		((RBNode<T>) parent.getParent()).setColour(Colour.RED);
+		if(parent.getLeft().equals(node)) {
+			Util.rightRotation((RBNode<T>) parent.getParent());
+		} else {
+			Util.leftRotation((RBNode<T>) parent.getParent());
+		}
 	}
 }
